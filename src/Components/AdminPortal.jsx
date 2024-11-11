0f0f78fd3d4573f9d419d687f2b572e21408bfad
Muhammad Mateen Amjad <112537229+bsce22005@users.noreply.github.com>
2024-11-11 10:07:04 +0500

    Second commit

--- src/Components/AdminPortal.jsx
+++ src/Components/AdminPortal.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import Navbar from './Nabar'
 import { Link } from 'react-router-dom'
+import NavbarA from './NabarA'
+import Chat from './Chat'
 
 function AdminPortal() {
   return (
     <>
-    <div className='row'>
-        <div className='col-3' style={{border:'2px solid black',height:'100vh',backgroundColor:"#4e4f51"}}>
-            <div style={{display:'flex',flexDirection:'column',textAlign:'center'}}>
-                <Link style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Messages</Link>
-                <Link style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Patients</Link>
-                <Link style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Doctors</Link>
-                <Link style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Fee Vouchers</Link>
-                <Link style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Generate Fee Voucher</Link>
-            </div>
-        </div>
-        <div className='col-9'  style={{border:'2px solid black',height:'100vh'}}></div>
-    </div>
+      <NavbarA/>
+      <Chat/>
     </>
   )
 }
--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,10 +1,14 @@
 import { io } from "socket.io-client";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 function Chat() {
+
+    const [message,setMessage]=useState({"text":""});
+    var socket;
   useEffect(() => {
     // Initialize the socket connection once when the component mounts
-    const socket = io("http://localhost:8000", {
+    socket = io("http://localhost:8000", {
       path: "/my-custom-path/"
     });
 
@@ -24,8 +28,35 @@ function Chat() {
     };
   }, []); // Empty dependency array to ensure this only runs on mount/unmount
 
+  const onch=(e)=>{
+    e.preventDefault();
+    setMessage({...message,[e.target.name]:e.target.value});
+
+  }
+
+  const sendMessage=()=>{
+    console.log(message);
+    socket.emit("AD",message);
+    socket.on("AD",(msg)=>{
+      console.log("Returned",msg);
+    })
+  }
+    
   return (
-    <div>Chat</div>
+    <div style={{display:'flex',flexDirection:'row'}}>
+        <div style={{border:'2px solid black',height:'100vh',backgroundColor:"#4e4f51",width:'30%',right:'0',paddingTop:'100px'}}>
+            <div style={{display:'flex',flexDirection:'column',textAlign:'center'}}>
+                <Link className="my-2" style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Doctor</Link>
+                <Link className="my-2" style={{color:'white',textDecoration:'none',border:'1px solid white',borderRadius:'10px',height:'40px',alignItems:'center',display:'flex',justifyContent:'center',fontWeight:'bold'}}>Guardian1</Link>
+            </div>
+        </div>
+        <div style={{border:'2px solid black',height:'100vh',width:'70%',right:'0'}}>
+          <div style={{bottom:'0',display:'flex',flexDirection:'row',marginTop:'530px'}}>
+          <input type="text"  name="text" onChange={onch} placeholder="Type a new message" style={{width:'70%',marginLeft:'70px'}}/>
+          <button className="btn btn-success mx-2" onClick={sendMessage}>Send</button>
+          </div>
+        </div>
+    </div>
   );
 }
 
